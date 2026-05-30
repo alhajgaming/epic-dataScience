842d75fc8e923510176d64258dec7464257fc6c9
alhajgaming <pathanalhajkhanabbaskhan@gmail.com>
2026-05-31 02:14:58 +0530

    final python file with comic style output

--- Matplotlib/Mats.py
+++ Matplotlib/Mats.py
@@ -1,4 +1,13 @@
 import matplotlib.pyplot as plt
-print("imported")
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+
+kohli = [0, 0, 500, 800, 1100, 1300, 1500, 1800, 1900, 2100]
+sehwag = [0, 300, 800, 1200, 1500, 1700, 1600, 1400, 1000, 0]
+years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
+runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
+
+with plt.xkcd():
+    plt.plot(years, kohli, color='orange', linestyle='--',marker=',', label="Kohli")
+    plt.plot(years, sehwag, color='green', linestyle='-.',marker='.', label="Sehwag")
+    plt.plot(years, runs, color='blue',linestyle='dashed',marker='^', label="Tendulkar")
+    plt.legend()
+    plt.show()
